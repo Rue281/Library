b34287b2778f9fbefb709af8c50b3eddde13166a
Remonda <Rue281@gmail.com>
2022-08-30 16:02:10 +0200

    changing form display logic

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let form = document.getElementById("book-form");
+let container = document.getElementsByClassName("container")[0];
+let booksContainer = document.getElementsByClassName("books-container")[0];
 let addBtn = document.getElementById("add");
+let iTag = addBtn.getElementsByClassName("fa-plus")[0];
 let submitBtn = document.getElementById("submitBtn");
 let title = document.getElementById("title");
 let author = document.getElementById("author");
@@ -24,17 +27,64 @@ let myLibrary = [];
 
 addBtn.addEventListener("click",function(){
     //When the user clicks the button, open the form
+    iTag.classList.add("fa-beat");
+    iTag.style = 
+    "--fa-animation-duration: 1s;--fa-animation-iteration-count: 1;";
     form.style.display = "block";
+    booksContainer.style.opacity = "0.1";
 });
 
 
+window.onload = function(){
+	
+    document.onclick = function(e){
+        if(e.target === container){
+            //close the form
+            form.style.display = "none";
+            //clear form fields
+            clear();
+            //remove animation-related classes
+            iTag.classList.remove("fa-beat");
+            iTag.style.removeProperty("--fa-animation-duration","--fa-animation-iteration-count");
+            booksContainer.style.opacity = "1";
+        }
+    };
+};
+
+
+
 // When the user clicks anywhere outside of the form, close it and clear form fields
-window.onclick = function(event) {
-    if (event.target == form) {
-      form.style.display = "none";
-      clear();
+/*window.onclick = function(event) {
+
+        if(event.target !==form){
+            //close the form
+            //form.style.display = "none";
+            //clear form fields
+            clear();
+            //remove animation-related classes
+            iTag.classList.remove("fa-beat");
+            iTag.style.removeProperty("--fa-animation-duration","--fa-animation-iteration-count");
+            console.log("event.target NOT EQUAL THE FORM");
+        }
+        else{
+            console.log("event.target == form");
+        }
+    
+
+    /*if (event.target == form) {
+
+        //remove animation class
+        iTag.classList.remove("fa-beat");
+        iTag.style.removeProperty("--fa-animation-duration","--fa-animation-iteration-count");
+        //form.style.removeProperty("position");
+
+        //if form display: block change it to none
+        form.style.display = "none";
+        //form.style.removeProperty("position");
+
+        clear();
     }
-  }
+  }*/
 
 //add books to myLibrary
 submitBtn.addEventListener("click",function(event){
@@ -66,7 +116,7 @@ function clear(){
 function createBooksGrid(title,author,numberOfPages,readCheckBox){
 let books = document.createElement("div");
 books.id = "books";
-document.body.appendChild(books);
+booksContainer.appendChild(books);
 
 let bookInfo = document.createElement("div");
 bookInfo.id = "book-info";
